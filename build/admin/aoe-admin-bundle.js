dd26845070848430fcdd61425d7dcbdfed9fb2b7
Treee <jonathon.lindemann@gmail.com>
2020-01-28 16:40:01 -0700

    changing websocket connect urls

--- build/admin/aoe-admin-bundle.js
+++ build/admin/aoe-admin-bundle.js
@@ -8,7 +8,7 @@ class AdminClient {
     constructor() {
         this.clientId = '';
         this.lastClickedCivs = [];
-        this.socket = new WebSocket('wss://ec2-52-11-210-14.us-west-2.compute.amazonaws.com:8443');
+        this.socket = new WebSocket('wss://ec2-34-216-67-54.us-west-2.compute.amazonaws.com:8443');
         this.socket.onopen = this.onOpen.bind(this);
         this.socket.onmessage = this.onMessage.bind(this);
         this.socket.onclose = this.onClose;
